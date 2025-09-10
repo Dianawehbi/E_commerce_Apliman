62940dc4a034097651753c55dda59be62a126c0d
Diana Wehbi <dianawehbiii@gmail.com>
2025-09-10 12:04:39 +0300

    Generate Backend Folder
    using Spring Boot

---
+++ backend-e-commerce/src/test/java/com/example/backend_e_commerce/BackendECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
