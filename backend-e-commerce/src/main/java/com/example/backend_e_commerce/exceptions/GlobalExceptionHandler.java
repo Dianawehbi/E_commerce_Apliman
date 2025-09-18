fdc796d3aa670c9d65a8841e8e0a2538e390ef8e
Diana Wehbi <dianawehbiii@gmail.com>
2025-09-18 13:26:45 +0300

    Update GlobalExceptionHandler.java

--- backend-e-commerce/src/main/java/com/example/backend_e_commerce/exceptions/GlobalExceptionHandler.java
+++ backend-e-commerce/src/main/java/com/example/backend_e_commerce/exceptions/GlobalExceptionHandler.java
@@ -3,26 +3,36 @@ package com.example.backend_e_commerce.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.backend_e_commerce.dto.ApiErrorResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ApiErrorResponse> handleNotFound(ResourceNotFoundException ex) {
+        return buildResponse(HttpStatus.NOT_FOUND , ex.getMessage()) ;
     }
 
     @ExceptionHandler(BusinessRuleException.class)
-    public ResponseEntity<String> handleBusinessRule(BusinessRuleException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<ApiErrorResponse> handleBusinessRule(BusinessRuleException ex) {
+        return buildResponse(HttpStatus.NOT_FOUND , ex.getMessage()) ;
     }
 
     @ExceptionHandler(InvalidRequestException.class)
-    public ResponseEntity<String> handleInvalidRequest(InvalidRequestException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<ApiErrorResponse> handleInvalidRequest(InvalidRequestException ex) {
+        return buildResponse(HttpStatus.NOT_FOUND , ex.getMessage()) ;
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneral(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    private ResponseEntity<ApiErrorResponse> buildResponse(HttpStatus status, String message) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(status.value(), status.getReasonPhrase(), message);
+        return new ResponseEntity<>(errorResponse, status);
     }
 
 }
